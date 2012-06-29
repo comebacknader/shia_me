21f71a61a5a6c19fe1159a2bb19d2dd3ff82c1a2
Nader Carun <comebacknader@hotmail.com>
2012-06-29 16:15:48 -0700

    Jquery to About Page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function() {
+	$(".about_image_privacy").hover(function(){
+			$(".about_match").css("display", "none");
+			$(".about_privacy").css("display", "block");
+	},
+	function() {
+			$(".about_privacy").css("display", "none");
+			$(".about_match").css("display", "block");
+	})
+	$(".about_image_join").hover(function(){
+			$(".about_match").css("display", "none");
+			$(".about_how_to").css("display", "block");			
+	},
+	function(){
+			$(".about_how_to").css("display", "none");
+			$(".about_match").css("display", "block");
+	});		
+});
