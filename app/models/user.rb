ad797ad1e86cadfc57af75f4be0606f5139a912e
Nader Carun <comebacknader@hotmail.com>
2012-07-30 12:58:13 -0400

    Added Email to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :location, :name
+  attr_accessible :bio, :location, :name, :email
+  
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true,
+                    length: { maximum: 70 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
+  validates :bio, presence: true
+  
 end
