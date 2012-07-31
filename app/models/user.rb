2da7a6044e8e2339e55312c8f2304b78328381f6
Nader Carun <comebacknader@hotmail.com>
2012-07-31 09:20:03 -0400

    Added Password and Password Conf to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :location, :name, :email, :gender
+  attr_accessible :bio, :location, :name, :email, :gender, :password, :password_confirmation
+  has_secure_password
+  
+  
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   
   validates :name, presence: true,
                    length: { maximum: 50 }
@@ -14,5 +19,16 @@ class User < ActiveRecord::Base
   validates :bio, presence: true
   
   validates :gender, presence: true
+  
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+  
+  validates :password_confirmation, presence: true
+  
+  private
+  
+    def create_remember_token 
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731113424) do
+ActiveRecord::Schema.define(:version => 20120731125632) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -45,12 +45,15 @@ ActiveRecord::Schema.define(:version => 20120731113424) do
     t.string   "name"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "email"
     t.string   "gender"
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
