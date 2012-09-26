fcb51de8097b2641085efe26f2cf50ceeec96c86
comebacknader <comebacknader@hotmail.com>
2012-09-25 17:44:28 -0700

    Added Logos to User All Bar and Container

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @question = @user.question
     @match = @user.matches.last
     @wmatch = @user.wmatches.last
   end
