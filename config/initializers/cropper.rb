a2e10b28aeb45d1bbc579808e2c081286aebc99d
comebacknader <comebacknader@hotmail.com>
2012-10-01 18:20:10 -0700

    trying blog code in cropper rb in config

--- config/initializers/cropper.rb
+++ config/initializers/cropper.rb
@@ -1,18 +1,23 @@
-module Paperclip
+odule Paperclip
   class Cropper < Thumbnail
-    def transformation_command
-      if crop_command
-        crop_command + super.first.sub(/ -crop \S+/, '').split(' ') # super returns an array like this: ["-resize", "100x", "-crop", "100x100+0+0", "+repage"]
-      else
-        super
-      end
+    def initialize(file, options = {}, attachment = nil)
+      super
+      @current_geometry.width  = target.crop_width
+      @current_geometry.height = target.crop_height
     end
-
-    def crop_command
-      target = @attachment.instance
-      if target.cropping?
-        ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}"]
-      end
+    def target
+      @attachment.instance
+    end
+    def transformation_command
+      crop_command = [
+        "-crop",
+        "#{target.crop_width}x" \
+          "#{target.crop_height}+" \
+          "#{target.crop_x}+" \
+          "#{target.crop_y}",
+        "+repage"
+      ]
+      crop_command + super
     end
   end
 end
