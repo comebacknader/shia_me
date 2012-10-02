d82ded52406c1f950a4d1dbf634b17068977d204
comebacknader <comebacknader@hotmail.com>
2012-10-01 19:11:23 -0700

    Added Some Comments' Code Stuff

--- config/initializers/cropper.rb
+++ config/initializers/cropper.rb
@@ -1,10 +1,8 @@
 module Paperclip
-  # Handles thumbnailing images that are uploaded.
   class Cropper < Thumbnail
-
     def transformation_command
       if crop_command
-        crop_command + super.sub(/ -crop \S+/, '')
+        crop_command + super.join(' ').sub(/ -crop \S+/, '').split(' ') # super returns an array like this: ["-resize", "100x", "-crop", "100x100+0+0", "+repage"]
       else
         super
       end
@@ -13,10 +11,8 @@ module Paperclip
     def crop_command
       target = @attachment.instance
       if target.cropping?
-        " -crop '#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}'"
+        ["-crop", "#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}"]
       end
     end
-
   end
-
 end
