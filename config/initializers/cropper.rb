2b744ccfc898930ca66f30076ea99c14cb4795a3
comebacknader <comebacknader@hotmail.com>
2012-10-01 18:30:07 -0700

    Not CAPS on module

--- config/initializers/cropper.rb
+++ config/initializers/cropper.rb
@@ -1,13 +1,17 @@
-Module Paperclip
+module Paperclip
+
   class Cropper < Thumbnail
+  
     def initialize(file, options = {}, attachment = nil)
       super
       @current_geometry.width  = target.crop_width
       @current_geometry.height = target.crop_height
     end
+    
     def target
       @attachment.instance
     end
+    
     def transformation_command
       crop_command = [
         "-crop",
