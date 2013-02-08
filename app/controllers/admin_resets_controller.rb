e139d2f255c30bc6f19a0e2c01c3b86accfaa95b
comebacknader <comebacknader@hotmail.com>
2013-02-07 21:30:31 -0800

    Added Edit and Update to Password Reset

--- app/controllers/admin_resets_controller.rb
+++ app/controllers/admin_resets_controller.rb
@@ -11,7 +11,21 @@ class AdminResetsController < ApplicationController
   	redirect_to adminreset_path, :notice => "Email Sent With Password Reset Instructions"
   end
 
+  def edit
+  	@admin = Admin.find_by_password_reset_token!(params[:id])
+  end
 
+  def update
+  	@admin = Admin.find_by_password_reset_token!(params[:id])
+  if @admin.password_reset_sent_at < 2.hours.ago
+  	redirect_to new_admin_reset_path, :alert => "Password Reset Has Expired"
+  elseif @admin.update_attributes(params[:admin])
+  	sign_in @admin
+  	redirect_to @admin, :notice => "Password Has Been Reset"
+  else 
+  	render "edit"
+  end
+ end
 end
 
 
